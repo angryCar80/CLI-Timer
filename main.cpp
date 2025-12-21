b731fb47a307bbbdd6232302af41d6d8269237b7
angryCar80 <ouabbasaymen@gmail.com>
2025-12-21 15:44:28 +0100

    feat: you choose between all the options and the timer is working

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
+#include <chrono>
 #include <iostream>
 #include <string>
 #include <termios.h>
+#include <thread>
 #include <unistd.h>
 #include <vector>
 
@@ -8,6 +10,12 @@
 #define reset "\033[0m"
 
 struct termios orig_termios;
+
+void wait() {
+  auto start = std::chrono::steady_clock::now();
+  std::this_thread::sleep_for(std::chrono::seconds(15));
+  auto end = std::chrono::steady_clock::now();
+}
 
 void disableRawMode() { tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios); }
 
@@ -26,9 +34,10 @@ void clearScreen() { std::cout << "\x1b[2J\x1b[H"; }
 int main() {
   enableRawMode();
 
-  std::vector<std::string> options = {"Seconds", "Minuts", "Houres"};
+  std::vector<std::string> options = {"Seconds", "Minutes", "Houres"};
 
   int selected = 0;
+  int timecount = 0;
 
   while (true) {
     clearScreen();
@@ -76,11 +85,26 @@ int main() {
     if (c == '\r') {
       clearScreen();
       disableRawMode();
-      int timecount = 0;
       std::cout << "You selected: " << options[selected] << "\n";
       std::cout << "How many " << options[selected] << ": ";
       std::cin >> timecount;
-      break;
+      // STARTING TIMER
+      auto start = std::chrono::steady_clock::now();
+      if (selected == 0) {
+        std::this_thread::sleep_for(std::chrono::seconds(timecount));
+      } else if (selected == 1) {
+        std::this_thread::sleep_for(std::chrono::minutes(timecount));
+      } else if (selected == 2) {
+        std::this_thread::sleep_for(std::chrono::hours(timecount));
+      }
+      auto end = std::chrono::steady_clock::now();
+
+      std::chrono::duration<float> elasped_time = end - start;
+
+      std::cout << "TIMER END: " << elasped_time.count() << options[selected]
+                << "passed";
+      // wait();
+      enableRawMode();
     }
   }
 
